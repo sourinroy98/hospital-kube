fa00b99a99102c0b36c49b198e59234dd54cdf41
sourinroy98 <Sourin.Roy@mindtree.com>
2022-09-19 19:58:34 +0530

    first commit

---
+++ src/main/java/com/springboot/test/HospitalManagementDataApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementDataApplication.class, args);
+	}
+
+}
